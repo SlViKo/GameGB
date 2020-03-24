77d36e0752b47983af5d6e2d116dd151c5f24847
SlViKo <41319845+SlViKo@users.noreply.github.com>
2020-03-24 19:21:40 +0300

    Revert "Передвижение иконки к точке клика"
    This reverts commit 9b583a0e39e98d2453affc45a10010b8316a0403.

--- core/src/com/mygdx/game/StartGame.java
+++ core/src/com/mygdx/game/StartGame.java
@@ -1,20 +1,4 @@
-package com.mygdx.game;
+package com.mygdx.game.screen;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.math.Vector2;
-import com.mygdx.game.screen.MenuScreen;
-
-
-public class StartGame extends Game {
-
-    @Override
-    public void create() {
-        setScreen(new MenuScreen());
-    }
+public class StartGame {
 }
