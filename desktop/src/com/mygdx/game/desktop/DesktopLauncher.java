7ad13790aa92ea898874d722cec46799f525e53d
SlViKo <41319845+SlViKo@users.noreply.github.com>
2020-03-29 11:57:09 +0300

    Правки зависимостей

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -2,7 +2,8 @@ package com.mygdx.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.mygdx.game.screen.StartGame;
+import com.mygdx.game.StartGame;
+
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
